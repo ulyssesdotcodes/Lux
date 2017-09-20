8c595102c8f94472601821fc2710fc9535106c3f
Ulysses Popple <ulysses.popple@gmail.com>
2017-09-20 19:39:43 -0400

    Build scripts

--- build.cmd
+++ build.cmd
@@ -1,4 +1,22 @@
+@echo off
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a_%%b)
+set datetime=%mydate%__%mytime%
+mkdir build\%datetime%
+
 cd web
 elm-make Main.elm --output index.html
 elm-make Admin.elm --output admin.html
-http-server
+
+cd ..
+
+robocopy /e .\web .\build\%datetime%\web
+robocopy /e .\TD .\build\%datetime%\TD /xf *.mp4 *.mov *.mp3 *.mpeg
+robocopy .stack-work\dist\ca59d0ab\build\Lux .\build\%datetime% Lux.exe 
+robocopy . .\build\%datetime% run.cmd 
+
+cd build
+
+7z a %datetime%.zip %datetime%\
+robocopy . "%HOME%\Google Drive\Lux\ " %datetime%.zip
